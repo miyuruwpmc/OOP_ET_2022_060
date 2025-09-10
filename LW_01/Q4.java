38d0d0ed63c15251f8ce82d683fb67e77fd0adb7
Miyuru Chathuranga <chathur-et22060@stu.kln.ac.lk>
2025-09-10 08:28:36 +0530

    Added Q4 solution

---
+++ LW_01/Q4.java
@@ -0,0 +1,15 @@
+package LW_01;
+
+import javax.swing.*;
+
+public class Q4 {
+    public static void main(String[] args) {
+        JFrame frame;
+        frame=new JFrame();
+        frame.setSize(300,200);
+        frame.setTitle("My First Frame");
+        frame.setLocation(100, 50);
+        frame.setVisible(true);
+
+    }
+}
